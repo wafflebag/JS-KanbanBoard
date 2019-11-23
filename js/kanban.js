8efa5f23e558f8486478151d3b33e21d63d860cc
wafflebag <40217899+wafflebag@users.noreply.github.com>
2019-11-22 17:38:44 -0800

    wip 3

--- js/kanban.js
+++ js/kanban.js
@@ -21,6 +21,9 @@ const create_item = () => {
   item.id = 'item-' + order;  
   item.draggable = true;
   item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id));
+  item.addEventListener('dragend', event => event.dataTransfer.clearData());
+  let input = document.createElement('input');
+  item.appendChild(input);
 };
 
 document.querySelectorAll('.drop').forEach(element => {
